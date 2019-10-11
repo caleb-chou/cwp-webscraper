fac1baa48b5bfb2ff63a732c4cb87dcda877dc69
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-10-11 16:27:43 -0500

    gets maxres youtube thumbnails

--- webscraper.py
+++ webscraper.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import re
 
-url = 'https://www.youtube.com/feed/trending'#str(input("Enter a website"))
-
+url = 'https://youtube.com'#str(input("Enter a website"))
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
 images = soup.find_all('img')
+counter = 0
+
+if not os.path.exists('./images'):
+    os.mkdir('./images')
 
 for img in images:
-    print(img['src'])
-    if(img['src'][-3:]):
-        print()
-    
+    if(img['src'][-3:]!= 'gif'):
+        path = './images/' + str(counter) + '.jpg'
+        with open(path, 'wb') as f:
+            if(img['src'][:4] == 'http' or not img['src'][:2] == '//'):
+                id = re.search('vi',str(img['src'])).end() + 1
+                response = requests.get('https://img.youtube.com/vi/' + img['src'][id:id+11] +  '/maxresdefault.jpg')
+            f.write(response.content);
+        counter+=1
+
