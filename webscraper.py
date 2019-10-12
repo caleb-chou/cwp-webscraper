e81741b02aee462489088ee26a8331e16ebb07de
caleb <haloisawesome412@gmail.com>
2019-10-12 12:44:28 -0500

    improved code

--- webscraper.py
+++ webscraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import re
 
-url = 'https://youtube.com'#str(input("Enter a website"))
+url = 'https://www.google.com'#str(input("Enter a website"))
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 images = soup.find_all('img')
@@ -16,9 +16,16 @@ for img in images:
     if(img['src'][-3:]!= 'gif'):
         path = './images/' + str(counter) + '.jpg'
         with open(path, 'wb') as f:
-            if(img['src'][:4] == 'http' or not img['src'][:2] == '//'):
+            if((img['src'][:4] == 'http' or not img['src'][:2] == '//') and not re.search('youtube.com',url) == None):
                 id = re.search('vi',str(img['src'])).end() + 1
                 response = requests.get('https://img.youtube.com/vi/' + img['src'][id:id+11] +  '/maxresdefault.jpg')
-            f.write(response.content);
+            else:
+                if(img['src'][:4] == 'http'):
+                    response = requests.get(img['src'])
+                elif(img['src'][:2]=='//'):
+                    response = requests.get('http:' + img['src'])
+                else:
+                    response = requests.get(url + img['src'])
+            f.write(response.content)
         counter+=1
 
